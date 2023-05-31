6d9484a9c3824933610bc7dd76da391b5e692708
alek <wait82985@gmail.com>
2023-05-31 14:49:07 +0600

    initial commit. 2 test cases

---
+++ src/main/java/org/example/Solution.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        //TODO
+        return 0;
+    }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,20 @@
+import org.example.Solution;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void findNumberOfLISTest1() {
+        int[] nums = {1, 3, 5, 4, 7};
+        int output = 2;
+        Assert.assertEquals(output, new Solution().findNumberOfLIS(nums));
+    }
+
+    @Test
+    public void findNumberOfLISTest2() {
+        int[] nums = {2, 2, 2, 2, 2};
+        int output = 5;
+        Assert.assertEquals(output, new Solution().findNumberOfLIS(nums));
+    }
+
+}
